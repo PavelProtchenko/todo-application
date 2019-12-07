70ba19368377077bfc814b3242f5f2d1dcc09994
PavelProtchenko <protchenkopa@gmail.com>
2019-12-07 21:48:04 +0300

    add input handlers

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const TodoForm: React.FC = () => {
+  const [title, setTitle] = useState<string>('')
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value)
+  }
+
+  const keyPressHandler = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      console.log(title)
+      setTitle('')
+    }
+  }
+
   return (
     <div className="input-field mt2">
-      <input type="text" id="title" placeholder="Enter name of todo" />
+      <input onChange={changeHandler} value={title} type="text" id="title" placeholder="Enter name of todo" onKeyPress={keyPressHandler} />
       <label htmlFor="title" className="active">
         Enter name of todo
       </label>
